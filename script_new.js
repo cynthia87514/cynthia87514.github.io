b01ee2306a56e89083b03c4dbbafc075c06dcf41
cynthia87514 <163651426+cynthia87514@users.noreply.github.com>
2024-03-18 23:52:57 +0800

    Add files via upload

---
+++ script_new.js
@@ -0,0 +1,112 @@
+function show1()
+{
+    var Q1 = document.getElementById('Q1');
+    var A1_1 = document.getElementById('A1_1');
+    var A1_2 = document.getElementById('A1_2');
+    var A1_3 = document.getElementById('A1_3');
+    if (A1_1.style.display === "none") {
+        A1_1.style.display = 'block';
+        A1_2.style.display = 'block';
+        A1_3.style.display = 'block';         
+    }
+    else {
+        A1_1.style.display = "none";
+        A1_2.style.display = 'none';
+        A1_3.style.display = 'none';
+    }
+}
+function show2()
+{
+    var Q2 = document.getElementById('Q2');
+    var A2_1 = document.getElementById('A2_1');
+    var A2_2 = document.getElementById('A2_2');
+    if (A2_1.style.display === "none") {
+        A2_1.style.display = 'block';
+        A2_2.style.display = 'block';        
+    }
+    else {
+        A2_1.style.display = "none";
+        A2_2.style.display = 'none';
+    }
+}
+function show3()
+{
+    var Q3 = document.getElementById('Q3');
+    var A3_1 = document.getElementById('A3_1');
+    var A3_2 = document.getElementById('A3_2');
+    if (A3_1.style.display === "none") {
+        A3_2.style.display = 'block';
+        A3_1.style.display = 'block';
+    }
+    else {
+        A3_1.style.display = "none";
+        A3_2.style.display = 'none';
+
+    }
+}
+function show4()
+{
+    var Q4 = document.getElementById('Q4');
+    var A4_1 = document.getElementById('A4_1');
+    var A4_2 = document.getElementById('A4_2');
+
+    if (A4_2.style.display === "none") {
+        A4_1.style.display = 'block';
+        A4_2.style.display = 'block';
+    }
+    else {
+        A4_1.style.display = "none";
+        A4_2.style.display = 'none';
+    }
+}
+function show5()
+{
+    var Q5 = document.getElementById('Q5');
+    var A5_1 = document.getElementById('A5_1');
+    var A5_2 = document.getElementById('A5_2');
+
+    if (A5_1.style.display === "none") {
+        A5_1.style.display = 'block';
+        A5_2.style.display = 'block';
+    }
+    else {
+        A5_1.style.display = "none";
+        A5_2.style.display = 'none';
+    }
+}
+function show6()
+{
+    var Q6 = document.getElementById('Q6');
+    var A6 = document.getElementById('A6');
+    if (A6.style.display === "none") {
+        A6.style.display = 'block'; 
+    }
+    else {
+        A6.style.display = "none";
+    }
+}
+function show7()
+{
+    var Q7 = document.getElementById('Q7');
+    var A7 = document.getElementById('A7');
+    if (A7.style.display === "none") {
+        A7.style.display = 'block';
+    }
+    else {
+        A7.style.display = "none";
+    }
+}
+function show8()
+{
+    var Q8 = document.getElementById('Q8');
+    var A8_1 = document.getElementById('A8_1');
+    var A8_2 = document.getElementById('A8_2');
+    if (A8_1.style.display === "none") {
+        A8_2.style.display = 'block';
+        A8_1.style.display = 'block';
+    }
+    else {
+        A8_1.style.display = "none";
+        A8_2.style.display = 'none';
+    }
+}
